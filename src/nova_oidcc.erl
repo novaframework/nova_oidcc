ccc9e551f2d33bd88260028f909da318b3c2f148
Daniel Widgren <daniel.widgren@gmail.com>
2024-02-17 15:26:23 +0100

    now only have one function to handle oidcc flow

--- src/nova_oidcc.erl
+++ src/nova_oidcc.erl
@@ -1,23 +1,8 @@
 -module(nova_oidcc).
 
--export([is_authorized/1]).
--export([get/3]).
--export([put/4]).
+-export([load_user_info/1]).
 
 -include_lib("oidcc/include/oidcc_token_introspection.hrl").
-
-get(_Type, _Token, _Req) -> miss.
-
-put(_Type, _Token, _Data, _Req) -> ok.
-is_authorized(Req) ->
-    case cowboy_req:headers(Req) of
-        #{<<"authorization">> := <<"Bearer ", Token/binary>>} ->
-            load_user_info(maps:put(oidcc_extract_authorization, Token, Req));
-        #{<<"authorization">> := Authorization} ->
-            send_invalid_header_response(Req, Authorization);
-        #{} ->
-            false
-    end.
 
 load_user_info(#{oidcc_extract_authorization := undefined}) ->
     false;
@@ -56,7 +41,7 @@ load_user_info(#{oidcc_extract_authorization := Token} = Req) ->
                     Cache:put(userinfo, Token, Claims, Req),
                     introspect_token(Req#{oidcc_load_userinfo => Claims});
                 {error, {http_error, 401, _Body}} ->
-                    send_inactive_token_response(maps:put(?MODULE, undefined, Req));
+                    {error, inactive_token, maps:put(?MODULE, undefined, Req)};
                 {error, Reason} ->
                     erlang:error(Reason)
             end
@@ -92,7 +77,7 @@ introspect_token(#{oidcc_extract_authorization := Token} = Req) ->
         {ok, #oidcc_token_introspection{active = true} = Introspection} ->
             validate_jwt_token(Req#{oidcc_introspect_token => Introspection});
         {ok, #oidcc_token_introspection{active = false} = Introspection} ->
-            send_inactive_token_response(Req#{oidcc_introspect_token => Introspection});
+            {error, inactive_token, Req#{oidcc_introspect_token => Introspection}};
         miss ->
             case
                 oidcc:introspect_token(
@@ -103,7 +88,7 @@ introspect_token(#{oidcc_extract_authorization := Token} = Req) ->
                     Cache:put(introspection, Token, Introspection, Req),
                     validate_jwt_token(Req#{oidcc_introspect_token => Introspection});
                 {ok, #oidcc_token_introspection{active = false} = Introspection} ->
-                    send_inactive_token_response(Req#{oidcc_introspect_token => Introspection});
+                    {error, inactive_token, Req#{oidcc_introspect_token => Introspection}};
                 {error, Reason} ->
                     erlang:error(Reason)
             end
@@ -135,35 +120,18 @@ validate_jwt_token(#{oidcc_extract_authorization := Token} = Req) ->
         {ok, ClientContext} ->
             case oidcc_token:validate_id_token(Token, ClientContext, any) of
                 {ok, Claims} ->
-                    {true, Req#{oidcc_validate_jwt_token => Claims}};
+                    {ok, Req#{oidcc_validate_jwt_token => Claims}};
                 {error, token_expired} ->
-                    send_inactive_token_response(maps:put(?MODULE, undefined, Req));
+                    {error, inactive_token, maps:put(?MODULE, undefined, Req)};
                 {error, Reason} ->
                     erlang:error(Reason)
             end;
         {error, token_expired} ->
-            send_inactive_token_response(maps:put(?MODULE, undefined, Req));
+            {error, inactive_token, maps:put(?MODULE, undefined, Req)};
         {error, Reason} ->
             erlang:error(Reason)
     end;
 validate_jwt_token(#{}) ->
     erlang:error(no_oidcc_cowboy_extract_authorization).
 
-send_inactive_token_response(Req0) ->
-    Req = cowboy_req:reply(
-        401,
-        #{<<"content-type">> => <<"text/plain">>},
-        <<"The provided token is inactive">>,
-        Req0
-    ),
-    {stop, Req}.
 
-send_invalid_header_response(Req0, GivenHeader) ->
-    Req = cowboy_req:reply(
-        400,
-        #{<<"content-type">> => <<"text/plain">>},
-        <<"Invalid authorization Header\n\nExpected: Authorization: Bearer <token>\nGiven: ",
-            GivenHeader/binary>>,
-        Req0
-    ),
-    {stop, Req}.
